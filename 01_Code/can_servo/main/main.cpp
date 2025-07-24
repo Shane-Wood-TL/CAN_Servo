14772fe4bedcc2df306b1a6f92bd6f7708419e05
Shane Wood <townlake101@gmail.com>
2025-07-24 13:10:02 -0400

    Update main.cpp

--- 01_Code/can_servo/main/main.cpp
+++ 01_Code/can_servo/main/main.cpp
@@ -12,9 +12,7 @@ void led_strip_driver_task(void *pv);
 
 float get_shortest_angle(float target, float current);
 
-#define WINDOW_SIZE 10
-#define MAX_ADC_FLOAT 4095.0f
-#define K_TO_C 273.15f
+
 
 
 SemaphoreHandle_t motor_speed_mutex;
@@ -87,8 +85,8 @@ adc_oneshot_chan_cfg_t adc_config = {
 
 
 
-#define max_raw_position 3297
-#define min_raw_position 0
+
+#define MOTOR_FREQUENCY 5000
 
 extern "C" void app_main(void)
 {
@@ -139,7 +137,7 @@ extern "C" void app_main(void)
         .speed_mode = LEDC_LOW_SPEED_MODE,
         .duty_resolution = LEDC_TIMER_12_BIT,
         .timer_num  = LEDC_TIMER_0,
-        .freq_hz    = 5000,
+        .freq_hz    = MOTOR_FREQUENCY,
         .clk_cfg = LEDC_AUTO_CLK,
         .deconfigure = false,   
     };
@@ -161,17 +159,17 @@ extern "C" void app_main(void)
 
 
 
-    xTaskCreate(read_temperature, "read_temperature", 2048, NULL, 5, NULL);
-    xTaskCreate(read_current, "read_current", 2048, NULL, 5, NULL);
-    xTaskCreate(read_position, "read_position", 2048, NULL, 5, NULL);
-    xTaskCreate(drive_motor, "drive_motor", 2048, NULL, 5, NULL);
-    xTaskCreate(can_bus, "can_bus", 10000, NULL, 5, NULL);
-    xTaskCreate(led_strip_driver_task, "led_strip_driver_task", 2048, NULL, 10, NULL);
+    xTaskCreate(read_temperature, "read_temperature", READ_TEMPERATURE_STACK_SIZE, NULL, READ_TEMPERATURE_PRIORITY, NULL);
+    xTaskCreate(read_current, "read_current", READ_CURRENT_STACK_SIZE, NULL, READ_CURRENT_PRIORITY, NULL);
+    xTaskCreate(read_position, "read_position", READ_POSITION_STACK_SIZE, NULL, READ_POSITION_PRIORITY, NULL);
+    xTaskCreate(drive_motor, "drive_motor", DRIVE_MOTOR_STACK_SIZE, NULL, DRIVE_MOTOR_PRIORITY, NULL);
+    xTaskCreate(can_bus, "can_bus", CAN_BUS_STACK_SIZE, NULL, CAN_BUS_PRIORITY, NULL);
+    xTaskCreate(led_strip_driver_task, "led_strip_driver_task", LED_STRIP_DRIVER_TASK_STACK_SIZE, NULL, LED_STRIP_DRIVER_TASK_PRIORITY, NULL);
         
 
 
     for(;;){
-        vTaskDelay(100);
+        vTaskDelay(MAIN_TASK_DELAY);
     }
 }
 
@@ -181,15 +179,21 @@ extern "C" void app_main(void)
 
 
 
+#define K_TO_C 273.15f
+#define THERMISTOR_RESISTOR_DIVIDER 10000.0f
+#define THERM_A 0.001129148f
+#define THERM_B 0.000234125f
+#define THERM_C 0.0000000876741f
+#define READ_TEMPERATURE_TASK_DELAY 1000
 void read_temperature(void *pv){
     int raw_temp_reading;
     for(;;){
        
         adc_oneshot_read(adc_handle, temp_sense_channel, &raw_temp_reading);
-        float temp_resistance = 10000.0f * ((MAX_ADC_FLOAT / (float)raw_temp_reading) - 1.0f);
+        float temp_resistance = THERMISTOR_RESISTOR_DIVIDER * ((MAX_ADC_FLOAT / (float)raw_temp_reading) - 1.0f);
 
         float logR2 = log(temp_resistance);
-        float T =  1.0f / ( 0.001129148 + ( 0.000234125 * logR2 ) + ( 0.0000000876741 * logR2 * logR2 * logR2) ) ;
+        float T =  1.0f / ( THERM_A + (THERM_B * logR2 ) + ( THERM_C * logR2 * logR2 * logR2) ) ;
         T = T - K_TO_C;
 
          xSemaphoreTake(temperature_mutex, portMAX_DELAY);
@@ -208,7 +212,7 @@ void read_temperature(void *pv){
         
         xSemaphoreGive(temperature_mutex);
 
-        vTaskDelay(pdMS_TO_TICKS(1000));
+        vTaskDelay(pdMS_TO_TICKS(READ_TEMPERATURE_TASK_DELAY));
     }
 }
 
@@ -221,17 +225,21 @@ void read_temperature(void *pv){
 
 
 
+#define MAX_RAW_POSITION 3297
+#define MIN_RAW_POSITION 0
+#define MAX_ANGLE 360.0f
+#define READ_POSITION_TASK_DELAY 10
 
 void read_position(void *pv){
     int raw_position_reading;
     for(;;){
         xSemaphoreTake(current_angle_velocity_mutex, portMAX_DELAY);
         adc_oneshot_read(adc_handle, position_channel, &raw_position_reading);
-        current_angle = raw_position_reading/(max_raw_position/360.0f);
+        current_angle = raw_position_reading/(MAX_RAW_POSITION/MAX_ANGLE);
         //printf("Angle: %f\n", current_angle);
         //read position
         xSemaphoreGive(current_angle_velocity_mutex);
-        vTaskDelay(pdMS_TO_TICKS(10));
+        vTaskDelay(pdMS_TO_TICKS(READ_POSITION_TASK_DELAY));
     }
 }
 
@@ -241,7 +249,7 @@ void read_position(void *pv){
 
 
 
-
+#define WINDOW_SIZE 10
 class running_average{
     private:
         float buffer[WINDOW_SIZE] = {0};
@@ -278,7 +286,7 @@ class running_average{
 
 
 
-
+#define READ_CURRENT_TASK_DELAY 30
 void read_current(void *pv){
     running_average current_average;
     for(;;){
@@ -306,9 +314,15 @@ float get_shortest_angle(float target, float current) {
 
 
 
+#define INTEGRAL_LIMIT 100.0f
+#define LOW_PASS_FILTER_COEFFICIENT 0.5f
 
-
-
+#define MAX_ADC_INT 4095
+#define DRIVE_MOTOR_TASK_DELAY 10
+#define INTEGRAL_ON_LARGE_CHANGE_RESET 10.0f
+#define INTEGRAL_LIMIT 100.0f
+#define ERROR_THRESHOLD 1.0f
+#define M_to_S 1000.0f
 
 void drive_motor(void *pv){
     // PID state
@@ -372,23 +386,21 @@ void drive_motor(void *pv){
                 //printf("Offset Value: %d\n", motor_offset_value);
                 //float error = target_angle - current_angle;
                 float error = get_shortest_angle(target_angle, current_angle);
-                float dt = 0.01f;  // time step in seconds
+                float dt = DRIVE_MOTOR_TASK_DELAY / M_to_S;  // time step in seconds
                 // Integral with anti-windup
                 integral += error * dt;
-                float integral_limit = 100.0f;  // limit integral
-                if (integral > integral_limit) integral = integral_limit;
-                else if (integral < -integral_limit) integral = -integral_limit;
+                if (integral > integral_limit) integral = INTEGRAL_LIMIT;
+                else if (integral < -integral_limit) integral = -INTEGRAL_LIMIT;
 
                 // Derivative
                 float derivative = (error - last_error) / dt;
-                float alpha = 0.5f; // Low-pass filter coefficient
                 static float last_filtered = 0.0f; // Last filtered value for derivative
-                float filtered_derivative = alpha * derivative + (1 - alpha) * last_filtered;
+                float filtered_derivative = LOW_PASS_FILTER_COEFFICIENT * derivative + (1 - LOW_PASS_FILTER_COEFFICIENT) * last_filtered;
 
                 last_error = error;
 
                 
-                if (fabsf(last_target - target_angle) > 10.0f) {
+                if (fabsf(last_target - target_angle) > INTEGRAL_ON_LARGE_CHANGE_RESET) {
                     integral = 0;
                 }
                 last_target = target_angle;
@@ -397,15 +409,15 @@ void drive_motor(void *pv){
                 float output = pid_P * error + pid_I * integral + pid_D * filtered_derivative;
                 xSemaphoreGive(PID_values_mutex);
 
-                if(output > 4095){
-                    last_motor_speed = 4095;
-                }else if(output < -4095){
-                    last_motor_speed = -4095;
+                if(output > MAX_ADC_INT){
+                    last_motor_speed = MAX_ADC_INT;
+                }else if(output < -MAX_ADC_INT){
+                    last_motor_speed = -MAX_ADC_INT;
                 }else{
                     last_motor_speed = (int16_t)output;
                 }
                 // Stop near target
-                if (fabsf(error) < 1.0f) {
+                if (fabsf(error) < ERROR_THRESHOLD) {
                     last_motor_speed = 0;
                     motor.break_motor();
                 }
@@ -419,7 +431,7 @@ void drive_motor(void *pv){
              motor.driveMotor(last_motor_speed);
         }
 
-        vTaskDelay(pdMS_TO_TICKS(10));        
+        vTaskDelay(pdMS_TO_TICKS(DRIVE_MOTOR_TASK_DELAY));        
     }
 }
 
@@ -439,6 +451,9 @@ void drive_motor(void *pv){
 
 
 
+#define TWAI_RX_QUEUE_SIZE 32
+#define TWAI_RX_QUEUE_SIZE 1
+#define CAN_BUS_TASK_DELAY 50
 
 void can_bus(void *pv){
 
@@ -461,8 +476,8 @@ void can_bus(void *pv){
         .rx_io = can_RX_pin,
         .clkout_io = TWAI_IO_UNUSED,
         .bus_off_io = TWAI_IO_UNUSED,
-        .tx_queue_len = 1,
-        .rx_queue_len = 32,
+        .tx_queue_len = TWAI_TX_QUEUE_SIZE,
+        .rx_queue_len = TWAI_RX_QUEUE_SIZE,
         .alerts_enabled = TWAI_ALERT_NONE,
         .clkout_divider = 0,
         .intr_flags = 0
@@ -484,7 +499,7 @@ void can_bus(void *pv){
     can_servo can_bus_driver(node_id);
     for(;;){
         can_bus_driver.receive_message();
-        vTaskDelay(pdMS_TO_TICKS(50));
+        vTaskDelay(pdMS_TO_TICKS(CAN_BUS_TASK_DELAY));
     }
 }
 
