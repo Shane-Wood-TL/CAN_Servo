48d5b618041c58b4a1faa4fb9c3dc59e79bcb460
Shane Wood <townlake101@gmail.com>
2025-07-28 13:58:05 -0400

    Update servo_info.h

--- 01_Code/can_servo/include/servo_info.h
+++ 01_Code/can_servo/include/servo_info.h
@@ -6,4 +6,14 @@
 #define version_major 0
 #define version_minor 1
 
+
+//#define CAN_25K
+//#define CAN_50K
+//#define CAN_100K
+//#define CAN_125K
+#define CAN_250K
+//#define CAN_500K
+//#define CAN_800K
+//#define CAN_1M
+
 #endif // SERVO_INFO_H
